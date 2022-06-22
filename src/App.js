74e5b602d6a5b09063d1d73666ee8019bef16100
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-06-22 02:19:56 -0700

    Creation of components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import { GlobalStyle } from "./GlobalStyle";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Home />
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+import NoImage from "../images/no_image.jpg";
+
+import { useHomeFetch } from "../hooks/useHomeFetch";
+
+const Home = () => {
+  const { state, loading, error } = useHomeFetch();
+  console.log(state);
+  return <div>Home Page</div>;
+};
+
+export default Home;
