937f4854ba38a12dd8cd170ad556e596ace00531
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-06-22 06:44:29 -0700

    Creation of Home page component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,27 @@ import React, { useState, useEffect } from "react";
 
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import NoImage from "../images/no_image.jpg";
+import HeroImage from "./HeroImage";
 
 import { useHomeFetch } from "../hooks/useHomeFetch";
 
 const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log(state);
-  return <div>Home Page</div>;
+  console.log(
+    `${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`
+  );
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
